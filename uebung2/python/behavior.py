7bbe622e4a01e5c69877e6fd21508ce486eb06bf
ndrebing <wurst@email>
2016-12-07 12:04:48 +0100

    too-close-detection implemented. crashes since there are no images as ppm. TODO: parse from camdata, execute every x seconds

--- uebung2/python/behavior.py
+++ uebung2/python/behavior.py
@@ -6,7 +6,12 @@ random.seed()
 
 lcam = []
 rcam = []
+#threshold for diameter that is considered to be close. Value distinguished by trial and error
+diameterTh = 25
 
+#coordinates for min and max coordinate of red pixel, used a diameter
+minX = 10000
+maxX = -10000
 state = "pt"
 width = 160
 height = 120
@@ -33,7 +38,7 @@ def approach(position):
     if(position[0] == "right"):
         right_actuator = 5.0
         left_actuator = 5.0 + float(position[1]) / float(width)*5
-        print("left actuator: " + str(left_actuator) + ", right actuator: " + str(right_actuator))
+        #print("left actuator: " + str(left_actuator) + ", right actuator: " + str(right_actuator))
 
 #checks for red stuff in cameraframe
 #could be modified, e.g. giving the color to search for as a parameter
@@ -41,44 +46,77 @@ def checkForRed():
     result = trackBall()
     return result[0] != "none"
 
+#calculates the diameter
+def diameter(minx, maxx):
+    return (maxx - minx)
+
 #checks wether or not a red pixel has been found. return tuple of (left or right frame as string, vertical pixelposition where center = 0)
+#also counts number of red pixels
 #could be extended so that the center of mass is returned
 def trackBall():
     global lcam, rcam
+    global minX,maxX
+    redPix = 0
+    red = False
     for i in range(len(lcam)):
         if lcam[i][0] >= 200 and lcam[i][1] <= 100 and lcam[i][2] <= 100:
             #move to zero as center
             approx_vert = i % width - width/2
-            print("rot")
+            if(approx_vert < minX):
+                minX = approx_vert
+            if(approx_vert > maxX):
+                maxX = approx_vert
+            red = True
+    if(red):
             return ("left", approx_vert)
     for i in range(len(rcam)):
         if rcam[i][0] >= 200 and rcam[i][1] <= 100 and rcam[i][2] <= 100:
             approx_vert = i % width - width/2
-            print("rot")
+            if(approx_vert < minX):
+                minX = approx_vert
+            if(approx_vert > maxX):
+                maxX = approx_vert
+            red = True
+    if(red):
             return ("right", approx_vert)
     return ("none", -1)
+
+def circle():
+    global left_actuator, right_actuator
+    left_actuator = 0.
+    right_actuator = 0.
 
 #simple state machine executing subfunctions
 def execute():
     global state
-    global right_actuator
-    global left_actuator
-    readData()
-    print(state)
+    global t
+    global right_actuator, left_actuator
+    global maxX,minX
+    minX = 10000
+    maxX = -10000
 
+    readData()
     if(state == "pt"):
         doPointturn()
         if(checkForRed() == True):
             print("switching to state approach, checkforred is true")
             state = "approach"
+            return
     if(state == "approach"):
         position = trackBall()
-        print(position[0])
-        if(position[0] == "none"):
+        print(diameter(minX, maxX))
+        if(diameter(minX, maxX) >= diameterTh):
+            print("now too close! switching to state circle")
+            state = "circle"
+            return
+        if(position[0] != "none"):
+            approach(position)
+        else:
             print("switching to state pt, position equals none")
             state = "pt"
-        else:
-            approach(position)
+            return
+    if(state == "circle"):
+        circle()
 
 #def readData(camData):
 #    global lcam, rcam
@@ -121,6 +159,7 @@ def doBehavior():
     #behavior = marsData["Config"]["Robotik2"]["behavior"]
 
     execute()
+
     #randomWalk(distance)
 
     # if timing(1):
