f9867723e48f0320a430defa3b286c3745d44caa
Nils Klaus <mail@wurstbrot>
2017-01-10 14:33:42 +0100

    astern tut ein bisschen was, aber noch nicht richtig

--- uebung3/behavior.py
+++ uebung3/behavior.py
@@ -4,14 +4,15 @@ import intersect
 import math
 import numpy as np
 
-goal = [6.,0.]
+goal = [4.,4.]
 nodes = []
 wps = []
 ini = False
 
 class Node:
     comingFrom = None
-    distance = 10000000.
+    sDistance = 10000000.
+    gDistance = 10000000.
     coords = [0.,0.]
     neighbours = []
 
@@ -28,59 +29,92 @@ class Node:
     #def expand(self):
 
 
-def calcNeighbours(pos):
-    global wps
+#def calcNeighbours(pos):
+#    global wps
+#    neighbours = []
+#    for wp in wps:
+#        #print "pos "+ str(pos) + " wp " + str(wp)
+#        if(not np.array_equal(wp, pos) and checkIntersect(pos, wp)):
+#            n = Node(wp)
+#            neighbours.append(n)
+#    return neighbours
+
+def calcNeighbourNodes(node):
+    global nodes
     neighbours = []
-    for wp in wps:
+    for n in nodes:
         #print "pos "+ str(pos) + " wp " + str(wp)
-        if(not np.array_equal(wp, pos) and checkIntersect(pos, wp)):
-            n = Node(wp)
+        if(not np.array_equal(n.coords, node.coords) and checkIntersect(n.coords, node.coords)):
             neighbours.append(n)
     return neighbours
 
 def initNodes():
-    global wps, goal
+    global wps, nodes, goal
     wps.append(goal)
     for wp in wps:
         newNode = Node(wp)
-        newNode.neighbours = calcNeighbours(newNode.coords)
         nodes.append(newNode)
+    for n in nodes:
+        n.neighbours = calcNeighbourNodes(n)
+
+#reconstruct from goalnode
+def constructPath(node):
+    i = node
+    path = []
+    path.append(np.array(i.coords))
+    while i.comingFrom != None:
+        #print i.comingFrom.coords
+        path.append(np.array(i.comingFrom.coords))
+        i = i.comingFrom
+    path.append(np.array(i.coords))
+    result = path[::-1]
+    return result
+
 
 def aStar(pos):
     global nodes, goal
+    path = []
     #print "goal is: " + str(goal)
     pos2d = [pos[0], pos[1]]
     openList = []
     closedList = []
     start = Node(pos2d)
-
-    start.neighbours = calcNeighbours(start.coords)
+    start.comingFrom = None
+    start.sDistance = 0
+    start.hDistance = dist(start.coords, goal)
+    start.neighbours = calcNeighbourNodes(start)
     openList.append(start)
 
     while openList:
-        current = min(openList, key=lambda x: x.distance)
+        current = min(openList, key=lambda x: x.hDistance)
         if current.coords == goal:
-            print "FOUND PATH"
-            return True
+            #print "FOUND PATH"
+            path = constructPath(current)
+            #print path
+            return path
         openList.remove(current)
+        #print len(openList)
         closedList.append(current)
-        print "current is: " + str(current.coords)
+        #print "current is: " + str(current.coords)
 
+        #print "nachbarn: " + str(len(current.neighbours))
         for n in current.neighbours:
             if n in closedList:
                 continue
-            tempDist = current.distance + dist(current.coords, n.coords)
+            tempDist = current.sDistance + dist(current.coords, n.coords)
 
             if n not in openList:
                 openList.append(n)
-            elif current.distance >= n.distance:
+            elif tempDist >= n.sDistance:
                 continue
 
             n.comingFrom = current
-            n.distance = tempDist
+            n.sDistance = tempDist
+            n.hDistance = n.sDistance + dist(n.coords, goal)
+
 
     #print "did not find a path"
-    return False
+    return path
 
 def checkIntersect(pos, wp):
     # for wall in walls:
@@ -94,24 +128,28 @@ def dist(p1, p2):
     return np.sum(np.square(np.subtract(p1, p2)))
 
 def doBehavior(distance, marsData, waypoints, walls, pos):
-    global ini, wps
+    global ini, wps, nodes
     if not ini:
         for wp in waypoints:
             wps.append([wp[0], wp[1]])
         initNodes()
-        print "initialized!!"
         ini = True
     #print "doBehavior: " + str(pos[0])
     clearLines("path")
     configureLines("path", 5, 0.2, 0.8, 0.2)
 
-    aStar(pos)
+    result = aStar(pos)
 
-    for wp in waypoints:
-        if checkIntersect(pos,wp):
-             #print str(pos[0]) + ":" + str(pos[1]) + " to " + str(wp[0]) + ":" + str(wp[1]) + ": no intersection"
-             appendLines("path", pos[0], pos[1], 0.5)
-             appendLines("path", wp[0], wp[1], 0.5)
+    for i in range(len(result)-1):
+        print result[i]
+        appendLines("path", result[i][0], result[i][1], 0.5)
+        appendLines("path", result[i+1][0], result[i+1][1], 0.5)
+
+#    for wp in waypoints:
+#        if checkIntersect(pos,wp):
+#             #print str(pos[0]) + ":" + str(pos[1]) + " to " + str(wp[0]) + ":" + str(wp[1]) + ": no intersection"
+#             appendLines("path", pos[0], pos[1], 0.5)
+#             appendLines("path", wp[0], wp[1], 0.5)
 
 
 
