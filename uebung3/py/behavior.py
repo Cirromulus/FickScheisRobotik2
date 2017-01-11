3d33bb071bfd8ecf57bbeae1aeca9ac636412d7c
ndrebing <ndrebing@informatik.uni-bremen.de>
2017-01-11 16:27:21 +0100

    abgabe fertig

--- uebung3/py/behavior.py
+++ uebung3/py/behavior.py
@@ -13,9 +13,9 @@ ini = False     #check if already initialized or not
 class Node:
     comingFrom = None   #reference to predecessor
     sDistance = 10000000.    #shortest path from start to goal
-    gDistance = 10000000.    #heuristical score. Used is the current shortest distance + euclidean distance to goal
+    gDistance = 10000000.    #heuristical score. 
     coords = [0.,0.]
-    neighbours = []     #neighbour nodes, where a neighbour is a node that is visible from predecessor
+    neighbours = []     #neighbour nodes
 
     def __init__(self, coords):
         self.coords = coords
@@ -32,7 +32,6 @@ def calcNeighbourNodes(node):
     global nodes
     neighbours = []
     for n in nodes:
-        #print "pos "+ str(pos) + " wp " + str(wp)
         if(not np.array_equal(n.coords, node.coords) and checkIntersect(n.coords, node.coords)):
             neighbours.append(n)
     return neighbours
@@ -53,7 +52,6 @@ def constructPath(node):
     path = []
     path.append(np.array(i.coords))
     while i.comingFrom != None:
-        #print i.comingFrom.coords
         path.append(np.array(i.comingFrom.coords))
         i = i.comingFrom
     path.append(np.array(i.coords))
@@ -77,25 +75,32 @@ def aStar(pos):
     openList.append(start)
 
     while openList:
+        #find node with lowest heuristic score
         current = min(openList, key=lambda x: x.hDistance)
         if current.coords == goal:
             #print "FOUND PATH"
             path = constructPath(current)
             return path
+        #remove from openList since it is going to be expanded now
         openList.remove(current)
         closedList.append(current)
 
         #EXPANSION of current node
         for n in current.neighbours:
+            #successor already checked, continue
             if n in closedList:
                 continue
+            #calculate distance travelled on this path
             tempDist = current.sDistance + dist(current.coords, n.coords)
 
+            #add neighbour to openList
             if n not in openList:
                 openList.append(n)
+            #dont update distance if path is longer than the one before
             elif tempDist >= n.sDistance:
                 continue
 
+            # update neighbour's data
             n.comingFrom = current
             n.sDistance = tempDist
             n.hDistance = n.sDistance + dist(n.coords, goal)
